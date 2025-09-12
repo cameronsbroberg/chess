258145a147419334265d9f44ad9a6e25087d6d1f
cameronsbroberg <cameronsbroberg@gmail.com>
2025-09-11 20:59:47 -0600

    Implemented Bishop movements. Two Tests are not passing but outputs match the expected moves list. Very strange. Will resolve later.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -11,7 +12,12 @@ import java.util.List;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,14 +36,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return this.pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return this.type;
     }
 
     /**
@@ -48,6 +54,83 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece.getPieceType() == PieceType.BISHOP) {
+            //return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null));
+            return getBishopMoves(board,myPosition,piece.pieceColor);
+        }
         return List.of();
+    }
+    private Collection<ChessMove> getBishopMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color){
+        Collection<ChessMove> validMoves = new ArrayList<>();
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+        while(row > 1 & col > 1){ //Will traverse towards the bottom left. FIXME check the edge cases
+            row--;
+            col--;
+            ChessPosition currentPosition = new ChessPosition(row,col);
+            if(board.getPiece(currentPosition) == null){
+                validMoves.add(new ChessMove(myPosition,currentPosition,PieceType.BISHOP));
+            }
+            else if(board.getPiece(currentPosition).getTeamColor() == color){
+                break;
+            }
+            else if(board.getPiece(currentPosition).getTeamColor() != color){
+                validMoves.add(new ChessMove(myPosition,currentPosition,PieceType.BISHOP)); //FIXME don't have your queens turn into bishops
+                break;
+            }
+        }
+        row = myPosition.getRow();
+        col = myPosition.getColumn();
+        while(row < 8 & col < 8){ //Will traverse towards the upper right.
+            row++;
+            col++;
+            ChessPosition currentPosition = new ChessPosition(row,col);
+            if(board.getPiece(currentPosition) == null){
+                validMoves.add(new ChessMove(myPosition,currentPosition,PieceType.BISHOP));
+            }
+            else if(board.getPiece(currentPosition).getTeamColor() == color){
+                break;
+            }
+            else if(board.getPiece(currentPosition).getTeamColor() != color){
+                validMoves.add(new ChessMove(myPosition,currentPosition,PieceType.BISHOP)); //FIXME don't have your queens turn into bishops
+                break;
+            }
+        }
+        row = myPosition.getRow();
+        col = myPosition.getColumn();
+        while(row > 1 & col < 8){ //will traverse towards lower right
+            row--;
+            col++;
+            ChessPosition currentPosition = new ChessPosition(row,col);
+            if(board.getPiece(currentPosition) == null){
+                validMoves.add(new ChessMove(myPosition,currentPosition,PieceType.BISHOP));
+            }
+            else if(board.getPiece(currentPosition).getTeamColor() == color){
+                break;
+            }
+            else if(board.getPiece(currentPosition).getTeamColor() != color){
+                validMoves.add(new ChessMove(myPosition,currentPosition,PieceType.BISHOP)); //FIXME don't have your queens turn into bishops
+                break;
+            }
+        }
+        row = myPosition.getRow();
+        col = myPosition.getColumn();
+        while(row < 8 & col > 1){ // will traverse towards upper left
+            row++;
+            col--;
+            ChessPosition currentPosition = new ChessPosition(row,col);
+            if(board.getPiece(currentPosition) == null){
+                validMoves.add(new ChessMove(myPosition,currentPosition,PieceType.BISHOP));
+            }
+            else if(board.getPiece(currentPosition).getTeamColor() == color){
+                break;
+            }
+            else if(board.getPiece(currentPosition).getTeamColor() != color){
+                validMoves.add(new ChessMove(myPosition,currentPosition,PieceType.BISHOP)); //FIXME don't have your queens turn into bishops
+                break;
+            }
+        }
+        return validMoves;
     }
 }
