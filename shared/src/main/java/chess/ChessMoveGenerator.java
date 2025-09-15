2cf3b58f07b26932aa7fcbdf982f880d5fbe9c0d
cameronsbroberg <cameronsbroberg@gmail.com>
2025-09-15 09:13:04 -0600

    Implemented Queen Moves. (Not a new class).

--- shared/src/main/java/chess/ChessMoveGenerator.java
+++ shared/src/main/java/chess/ChessMoveGenerator.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class ChessMoveGenerator {
@@ -8,6 +9,14 @@ public class ChessMoveGenerator {
             return new BishopMoveGenerator().getMoves(board,myPosition);
         }if (board.getPiece(myPosition).getPieceType() == ChessPiece.PieceType.ROOK){
             return new RookMoveGenerator(board, myPosition).getMoves();
+        }
+        if (board.getPiece(myPosition).getPieceType() == ChessPiece.PieceType.QUEEN){
+            Collection<ChessMove> queenMoves = new ArrayList<>();
+            Collection<ChessMove> bishopMoves = new BishopMoveGenerator().getMoves(board,myPosition);
+            Collection<ChessMove> rookMoves = new RookMoveGenerator(board,myPosition).getMoves();
+            queenMoves.addAll(bishopMoves);
+            queenMoves.addAll(rookMoves);
+            return queenMoves;
         }
         return null;
     }
