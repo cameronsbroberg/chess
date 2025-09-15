2f95b7df9455426044774f87cbec2c31060ee987
cameronsbroberg <cameronsbroberg@gmail.com>
2025-09-15 16:27:29 -0600

    Updated Bishop Moves to match other classes format.

--- shared/src/main/java/chess/BishopMoveGenerator.java
+++ shared/src/main/java/chess/BishopMoveGenerator.java
@@ -4,74 +4,85 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class BishopMoveGenerator {
-    public Collection<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition){
-        Collection<ChessMove> validMoves = new ArrayList<>();
-        ChessGame.TeamColor color = board.getPiece(myPosition).getTeamColor();
-        int row = myPosition.getRow();
-        int col = myPosition.getColumn();
+    private final ChessBoard board;
+    private final ChessPosition startingPosition;
+    private int row;
+    private int col;
+    private Collection<ChessMove> validMoves = new ArrayList<>();
+    private final ChessGame.TeamColor color;
+
+    public BishopMoveGenerator(ChessBoard board, ChessPosition startingPosition) {
+        this.board = board;
+        this.startingPosition = startingPosition;
+        this.color = board.getPiece(startingPosition).getTeamColor();
+        this.row = startingPosition.getRow();
+        this.col = startingPosition.getColumn();
+    }
+
+    public Collection<ChessMove> getMoves(){
         while(row > 1 & col > 1){ //Will traverse towards the bottom left. FIXME check the edge cases
             row--;
             col--;
             ChessPosition currentPosition = new ChessPosition(row,col);
             if(board.getPiece(currentPosition) == null){
-                validMoves.add(new ChessMove(myPosition,currentPosition,null));
+                validMoves.add(new ChessMove(startingPosition,currentPosition,null));
             }
             else if(board.getPiece(currentPosition).getTeamColor() == color){
                 break;
             }
             else if(board.getPiece(currentPosition).getTeamColor() != color){
-                validMoves.add(new ChessMove(myPosition,currentPosition,null));
+                validMoves.add(new ChessMove(startingPosition,currentPosition,null));
                 break;
             }
         }
-        row = myPosition.getRow();
-        col = myPosition.getColumn();
+        row = startingPosition.getRow();
+        col = startingPosition.getColumn();
         while(row < 8 & col < 8){ //Will traverse towards the upper right.
             row++;
             col++;
             ChessPosition currentPosition = new ChessPosition(row,col);
             if(board.getPiece(currentPosition) == null){
-                validMoves.add(new ChessMove(myPosition,currentPosition,null));
+                validMoves.add(new ChessMove(startingPosition,currentPosition,null));
             }
             else if(board.getPiece(currentPosition).getTeamColor() == color){
                 break;
             }
             else if(board.getPiece(currentPosition).getTeamColor() != color){
-                validMoves.add(new ChessMove(myPosition,currentPosition,null));
+                validMoves.add(new ChessMove(startingPosition,currentPosition,null));
                 break;
             }
         }
-        row = myPosition.getRow();
-        col = myPosition.getColumn();
+        row = startingPosition.getRow();
+        col = startingPosition.getColumn();
         while(row > 1 & col < 8){ //will traverse towards lower right
             row--;
             col++;
             ChessPosition currentPosition = new ChessPosition(row,col);
             if(board.getPiece(currentPosition) == null){
-                validMoves.add(new ChessMove(myPosition,currentPosition,null));
+                validMoves.add(new ChessMove(startingPosition,currentPosition,null));
             }
             else if(board.getPiece(currentPosition).getTeamColor() == color){
                 break;
             }
             else if(board.getPiece(currentPosition).getTeamColor() != color){
-                validMoves.add(new ChessMove(myPosition,currentPosition,null));
+                validMoves.add(new ChessMove(startingPosition,currentPosition,null));
                 break;
             }
         }
-        row = myPosition.getRow();
-        col = myPosition.getColumn();
+        row = startingPosition.getRow();
+        col = startingPosition.getColumn();
         while(row < 8 & col > 1){ // will traverse towards upper left
             row++;
             col--;
             ChessPosition currentPosition = new ChessPosition(row,col);
             if(board.getPiece(currentPosition) == null){
-                validMoves.add(new ChessMove(myPosition,currentPosition,null));
+                validMoves.add(new ChessMove(startingPosition,currentPosition,null));
             }
             else if(board.getPiece(currentPosition).getTeamColor() == color){
                 break;
             }
             else if(board.getPiece(currentPosition).getTeamColor() != color){
-                validMoves.add(new ChessMove(myPosition,currentPosition,null));
+                validMoves.add(new ChessMove(startingPosition,currentPosition,null));
                 break;
             }
         }
--- shared/src/main/java/chess/ChessMoveGenerator.java
+++ shared/src/main/java/chess/ChessMoveGenerator.java
@@ -6,13 +6,13 @@ import java.util.Collection;
 public class ChessMoveGenerator {
     public Collection<ChessMove> generatePieceMoves(ChessBoard board, ChessPosition myPosition){
         if (board.getPiece(myPosition).getPieceType() == ChessPiece.PieceType.BISHOP){
-            return new BishopMoveGenerator().getMoves(board,myPosition);
+            return new BishopMoveGenerator(board,myPosition).getMoves();
         }if (board.getPiece(myPosition).getPieceType() == ChessPiece.PieceType.ROOK){
             return new RookMoveGenerator(board, myPosition).getMoves();
         }
         if (board.getPiece(myPosition).getPieceType() == ChessPiece.PieceType.QUEEN){
             Collection<ChessMove> queenMoves = new ArrayList<>();
-            Collection<ChessMove> bishopMoves = new BishopMoveGenerator().getMoves(board,myPosition);
+            Collection<ChessMove> bishopMoves = new BishopMoveGenerator(board,myPosition).getMoves();
             Collection<ChessMove> rookMoves = new RookMoveGenerator(board,myPosition).getMoves();
             queenMoves.addAll(bishopMoves);
             queenMoves.addAll(rookMoves);
