507efe13cb0066f6a297a6f6cdd123ea4fbc5c81
cameronsbroberg <cameronsbroberg@gmail.com>
2025-10-30 12:11:09 -0600

    Implemented getUser() and tests

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -10,45 +10,51 @@ public class MySqlUserDAO implements UserDAO{
         try {
             configureDatabase();
         } catch (DataAccessException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e); //FIXME: this should not throw a Runtime Exception
         }
     }
-    private final String[] createStatements = {
-            """
-            CREATE TABLE IF NOT EXISTS user(
-            id INT NOT NULL AUTO_INCREMENT,
-            username VARCHAR(255) NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            email VARCHAR(255) NOT NULL,
-            PRIMARY KEY (id)
-            );
-            """,
-            """
-            CREATE TABLE IF NOT EXISTS authData (
-            authToken VARCHAR(255) NOT NULL,
-            username VARCHAR(255) NOT NULL,
-            PRIMARY KEY (authToken)
-            );
-            """,
-
-            """
-            CREATE TABLE IF NOT EXISTS gameData (
-            gameID INT NOT NULL AUTO_INCREMENT,
-            whiteUsername VARCHAR(255),
-            blackUsername VARCHAR(255),
-            gameName VARCHAR(255) NOT NULL,
-            game VARCHAR(255) NOT NULL,
-            PRIMARY KEY (gameID)
-            );
-            """
-    };
+//    private final String[] createStatements = {
+//            """
+//            CREATE TABLE IF NOT EXISTS user(
+//            id INT NOT NULL AUTO_INCREMENT,
+//            username VARCHAR(255) NOT NULL,
+//            password VARCHAR(255) NOT NULL,
+//            email VARCHAR(255) NOT NULL,
+//            PRIMARY KEY (id)
+//            );
+//            """,
+//            """
+//            CREATE TABLE IF NOT EXISTS authData (
+//            authToken VARCHAR(255) NOT NULL,
+//            username VARCHAR(255) NOT NULL,
+//            PRIMARY KEY (authToken)
+//            );
+//            """,
+//
+//            """
+//            CREATE TABLE IF NOT EXISTS gameData (
+//            gameID INT NOT NULL AUTO_INCREMENT,
+//            whiteUsername VARCHAR(255),
+//            blackUsername VARCHAR(255),
+//            gameName VARCHAR(255) NOT NULL,
+//            game VARCHAR(255) NOT NULL,
+//            PRIMARY KEY (gameID)
+//            );
+//            """
+//    };
     private void configureDatabase() throws DataAccessException{
         DatabaseManager.createDatabase();
         try(var conn = DatabaseManager.getConnection()){
-            for(var statement : createStatements){
-                try(var preparedStatement = conn.prepareStatement(statement)){
-                    preparedStatement.executeUpdate();
-                }
+            String statement =             """
+            CREATE TABLE IF NOT EXISTS user(
+            username VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            PRIMARY KEY (username)
+            );
+            """;
+            try(var preparedStatement = conn.prepareStatement(statement)){
+                preparedStatement.executeUpdate();
             }
         }
         catch (SQLException e) {
@@ -56,37 +62,53 @@ public class MySqlUserDAO implements UserDAO{
         }
     }
     @Override
-    public void createUser(UserData userData) {
-
+    public void createUser(UserData userData) throws ResponseException{
+        try (var conn = DatabaseManager.getConnection()){
+            String statement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?);";
+            try(var preparedStatement = conn.prepareStatement(statement)){
+                preparedStatement.setString(1, userData.username());
+                preparedStatement.setString(2, userData.password());
+                preparedStatement.setString(3, userData.email());
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new ResponseException(e.getMessage());
+        } catch (DataAccessException e) {
+            throw new ResponseException(e.getMessage());
+        }
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
         try (var conn = DatabaseManager.getConnection()){
-            String statement = "SELECT id, username, password, email FROM user WHERE username ='" + username + "'";
+            String statement = "SELECT password, email FROM user WHERE username = ?";
             try(var preparedStatement = conn.prepareStatement(statement)){
-                preparedStatement.executeUpdate();
+                preparedStatement.setString(1,username);
+                try(var response = preparedStatement.executeQuery()){
+                    UserData userData = null;
+                    while(response.next()){
+                        String password = response.getString("password");
+                        String email = response.getString("email");
+                        userData = new UserData(username,password,email);
+                    }
+                    if(userData == null){
+                        throw new DataAccessException("Username not found");
+                    }
+                    return userData;
+                }
             }
         }
-        catch(Exception e){
-            throw new DataAccessException("Username not found");
+        catch(SQLException e){
+            throw new ResponseException(e.getMessage());
         }
-        return null;
     }
 
     @Override
     public void clear() throws ResponseException{ //FIXME this should probably throw a ResponseException
-        String[] deleteStatements = {//TODO: Should this be delete? Or Truncate?
-                //TODO: Separate tables by DAO. This should be a single statement instead of a list.
-                "TRUNCATE TABLE user;",
-                "TRUNCATE TABLE authData;",
-                "TRUNCATE TABLE game;"
-        };
         try(var conn = DatabaseManager.getConnection()){
-            for(var statement : deleteStatements){
-                try(var preparedStatement = conn.prepareStatement(statement)){
-                    preparedStatement.executeUpdate();
-                }
+            String statement = "TRUNCATE TABLE user"; //TODO: Should this be truncate instead?
+            try(var preparedStatement = conn.prepareStatement(statement)){
+                preparedStatement.executeUpdate();
             }
         }
         catch (SQLException | DataAccessException e) {//FIXME you should probably deal with this somewhere else.
--- server/src/test/java/database/DAOTests.java
+++ server/src/test/java/database/DAOTests.java
@@ -1,14 +1,18 @@
 package database;
 
+import dataaccess.DataAccessException;
 import dataaccess.DatabaseManager;
 import dataaccess.MySqlUserDAO;
 import model.UserData;
 import org.eclipse.jetty.server.Authentication;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import server.Server;
 
 public class DAOTests {
-    private static final UserData TEST_USER = new UserData("ExistingUser", "existingUserPassword", "eu@mail.com");
+    private static final UserData TEST_USER = new UserData("SuitcaseMurphy", "smPassword", "sm@mail.com");
 
     private static Server server;
 
@@ -19,16 +23,39 @@ public class DAOTests {
         System.out.println("Started test HTTP server on " + port);
     }
 
+    @Test
+    @DisplayName("Start a database")
     public void initializationTest() {
+        MySqlUserDAO userDAO = Assertions.assertDoesNotThrow(() -> new MySqlUserDAO());
+    }
+    @Test
+    @DisplayName("Delete a database")
+    public void destructionTest() {
         MySqlUserDAO userDAO = new MySqlUserDAO();
+        userDAO.clear();
+    }
+    @Test
+    @DisplayName("Insert a user successfully")
+    public void insertUserTest(){
+        MySqlUserDAO userDAO = new MySqlUserDAO();
+        userDAO.clear();
+        Assertions.assertDoesNotThrow(() -> userDAO.createUser(TEST_USER));
 
-        //TODO I guess this stuff shouldn't go here. My code should still be functional and I should have the SQL-accessing portions in my DAOs.
-//        try(var conn = DatabaseManager.getConnection()){
-//            for(var statement : queries){
-//                try(var preparedStatement = conn.prepareStatement(statement)){
-//                    preparedStatement.executeUpdate();
-//                }
-//            }
-//        }
+    }
+    @Test
+    @DisplayName("Get a user after inserting")
+    public void getUser() {
+        MySqlUserDAO userDAO = new MySqlUserDAO();
+        userDAO.clear();
+        Assertions.assertDoesNotThrow(() -> userDAO.createUser(TEST_USER));
+        UserData resultUser = Assertions.assertDoesNotThrow(() -> userDAO.getUser(TEST_USER.username()));
+        Assertions.assertEquals(resultUser, TEST_USER);
+    }
+    @Test
+    @DisplayName("Get a user that's not there")
+    public void getBadUser(){
+        MySqlUserDAO userDAO = new MySqlUserDAO();
+        userDAO.clear();
+        Assertions.assertThrows(DataAccessException.class, () -> userDAO.getUser("fakeUser"));
     }
 }
